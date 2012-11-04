a6fb6ceca9adf3ae5f4e78b55338333a159135bc
lefty313 <lewy313@gmail.com>
2012-11-04 20:11:46 +0100

    generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PragmaticPageGenerator::Application.config.secret_token = '6d8cbd34468e944150ce483844cdd2813abcc161c273ade09303c4d2cee8b4e3471da49631854d2be83a38e94076584e2082735a97adf016e7fcf11829645fbe'
